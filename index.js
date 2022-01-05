ac78fff8817b64ad92e481535e5f44b4bc4133ca
Bilal Ehsan <bilalprogrammer09@gmail.com>
2022-01-05 19:02:02 +0500

    cors origin edit

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const assignment = require("./modules/assignment/assignmentRoutes");
 require("dotenv").config();
 const port = process.env.PORT || 4040;
 app.use(cors({
-	origin: ["http://localhost:3000", "https://school1.surge.sh"],
+	origin: "*",
+	// origin: ["http://localhost:3000", "https://school1.surge.sh"],
 	credentials: true,
 }));
 
